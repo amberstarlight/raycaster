81eb75abca905e2e8e8c28cebede865db69f14e0
Amber Holly <halcyonsilver@gmail.com>
2020-06-15 19:16:14 +0100

    Update variable names
    - Rename variables for readability

--- main.c
+++ main.c
@@ -4,33 +4,17 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
-#define PI M_PI
-#define P2 (PI/2)
-#define P3 (3*PI/2)
-#define DR ((2 * PI)/360) // 0.0174533 // one degree in radians
-// 2 * pi Rads = 360 Degs
-// (2 * pi)/360 Rads = 1 Degs
+#define PI 3.14159265358979323846
+#define DEGREE ((2 * PI)/360) // one degree in radians
 
-#define WIDTH 1024
-#define HEIGHT 512
+#define VIEWPORT_WIDTH 1024
+#define VIEWPORT_HEIGHT 512
 
-float px, py, pdx, pdy, pa; // player Pos
+// Map
+const int mapX = 8,
+          mapY = 8,
+          mapSize = mapX * mapY;
 
-void drawPlayer() {
-  glColor3f(0, 1, 0);
-  glPointSize(8);
-  glBegin(GL_POINTS);
-  glVertex2i(px, py);
-  glEnd();
-
-  glLineWidth(3);
-  glBegin(GL_LINES);
-  glVertex2i(px, py);
-  glVertex2i(px + pdx * 5, py + pdy * 5);
-  glEnd();
-}
-
-int mapX=8, mapY=8, mapS=64;
 int map[] = {
   1,1,1,1,1,1,1,1,
   1,0,1,0,0,0,0,1,
@@ -42,8 +26,25 @@ int map[] = {
   1,1,1,1,1,1,1,1,
 };
 
+// Player position, deltas, and angle
+float playerX, playerY, playerDeltaX, playerDeltaY, playerAngle;
+
+void drawPlayer() {
+  glColor3f(0, 1, 0);
+  glPointSize(8);
+  glBegin(GL_POINTS);
+  glVertex2i(playerX, playerY);
+  glEnd();
+
+  glLineWidth(3);
+  glBegin(GL_LINES);
+  glVertex2i(playerX, playerY);
+  glVertex2i(playerX + playerDeltaX * 5, playerY + playerDeltaY * 5);
+  glEnd();
+}
+
 void drawMap2D() {
-  int x, y, xo, yo;
+  int x, y, xOffset, yOffset;
 
   for (y = 0; y < mapY; y++) {
    for (x = 0; x < mapX; x++) {
@@ -53,168 +54,172 @@ void drawMap2D() {
        glColor3f(0, 0, 0);
      }
 
-     xo = x*mapS;
-     yo = y*mapS;
+     xOffset = x * mapSize;
+     yOffset = y * mapSize;
 
      glBegin(GL_QUADS);
-     glVertex2i(xo + 1, yo + 1);
-     glVertex2i(xo + 1, yo + mapS - 1);
-     glVertex2i(xo + mapS - 1, yo + mapS - 1);
-     glVertex2i(xo + mapS - 1, yo + 1);
+     glVertex2i(xOffset + 1, yOffset + 1);
+     glVertex2i(xOffset + 1, yOffset + mapSize - 1);
+     glVertex2i(xOffset + mapSize - 1, yOffset + mapSize - 1);
+     glVertex2i(xOffset + mapSize - 1, yOffset + 1);
      glEnd();
    }
-  }
-  
+  }  
 }
 
-float dist(float ax, float ay, float bx, float by, float ang) {
-  return (sqrt((bx - ax) * (bx - ax) + (by-ay) * (by - ay)) );
+// pythagorean theorem
+//     /|
+//  c /_| b
+//     a
+
+float dist(float ax, float ay, float bx, float by) {
+  return ( sqrt((bx - ax) * (bx - ax) + (by-ay) * (by - ay)) );
 }
 
-void drawRays2D() {
-  int r, mx, my, mp, dof;
-  float rx, ry, ra, xo, yo, disT;
+void castRays() {
+  int ray, mapXPos, mapYPos, mapArrPos, dof;
+  float rayX = 0, rayY = 0, rayAngle = 0, xOffset = 0, yOffset = 0, distance = 0;
 
-  ra = pa - DR * 30;
-  if (ra < 0) { ra += 2 * PI; }
-  if (ra > 2 * PI) { ra -= 2 * PI; }
+  rayAngle = playerAngle - DEGREE * 30;
+  if (rayAngle < 0) { rayAngle += (2 * PI); }
+  if (rayAngle > (2 * PI)) { rayAngle -= (2 * PI); }
 
-  for (r = 0; r < 60; r++) {
+  for (ray = 0; ray < 60; ray++) {
     // Horizontal Line Check
     dof = 0;
-    float disH = 10000000;
-    float hx = px;
-    float hy = py;
-    float aTan = -1/tan(ra);
+    float rayDistHor = 10000000;
+    float horRayX = playerX;
+    float horRayY = playerY;
+    float aTan = -1/tan(rayAngle); // inverse tangent
 
-    if (ra > PI) { // ray looking up
-      ry = (((int)py >> 6) << 6) - 0.0001;
-      rx = (py - ry) * aTan + px;
-      yo = -64;
-      xo = -yo * aTan;
+    if (rayAngle > PI) { // ray looking up
+      rayY = (((int)playerY >> 6) << 6) - 0.0001; // round to nearest multiple of 64 (and subtract for accuracy)
+      rayX = (playerY - rayY) * aTan + playerX;
+      yOffset = -64;
+      xOffset = -yOffset * aTan;
     }
 
-    if (ra < PI) { // ray looking down
-      ry = (((int)py >> 6) << 6) + 64;
-      rx = (py - ry) * aTan + px;
-      yo = 64;
-      xo = -yo * aTan;
+    if (rayAngle < PI) { // ray looking down
+      rayY = (((int)playerY >> 6) << 6) + 64;
+      rayX = (playerY - rayY) * aTan + playerX;
+      yOffset = 64;
+      xOffset = -yOffset * aTan;
     }
 
-    if (ra == 0 || ra == PI) { // ray directly left or right
-      rx = px;
-      ry = py;
+    if (rayAngle == 0 || rayAngle == PI) { // ray directly left or right
+      rayX = playerX;
+      rayY = playerY;
       dof = 8;
     }
 
     while (dof < 8) {
-      mx = (int)(rx)>>6;
-      my = (int)(ry)>>6;
-      mp = my * mapX + mx;
+      mapXPos = (int)(rayX) >> 6;
+      mapYPos = (int)(rayY) >> 6;
+      mapArrPos = mapYPos * mapX + mapXPos;
 
-      if (mp >= 0 && mp < mapX * mapY && map[mp] == 1) {// hit a wall
-        hx = rx;
-        hy = ry;
-        disH = dist(px, py, hx, hy, ra);
+      if (mapArrPos >= 0 && mapArrPos < (mapX * mapY) && map[mapArrPos] == 1) { // hit a horizontal wall
+        horRayX = rayX;
+        horRayY = rayY;
+        rayDistHor = dist(playerX, playerY, horRayX, horRayY);
         dof = 8;
       } else {
-        rx += xo;
-        ry += yo;
+        rayX += xOffset;
+        rayY += yOffset;
         dof += 1; // next line
       }
     }
 
     // Vertical Line Check
     dof = 0;
-    float disV = 1000000;
-    float vx = px;
-    float vy = py;
-    float nTan = -tan(ra);
+    float rayDistVer = 1000000;
+    float vertRayX = playerX;
+    float vertRayY = playerY;
+    float nTan = -tan(rayAngle);
 
-    if (ra > P2 && ra < P3) { // ray looking left
-      rx = (((int)px >> 6) << 6) - 0.0001;
-      ry = (px - rx) * nTan + py;
-      xo = -64;
-      yo = -xo * nTan;
+    if ((rayAngle > (PI / 2)) && (rayAngle < (3*PI/2))) { // ray looking left
+      rayX = (((int)playerX >> 6) << 6) - 0.0001;
+      rayY = (playerX - rayX) * nTan + playerY;
+      xOffset = -64;
+      yOffset = -xOffset * nTan;
     }
 
-    if (ra < P2 || ra > P3) { // ray looking right
-      rx = (((int)px >> 6) << 6) + 64;
-      ry = (px - rx) * nTan + py;
-      xo = 64;
-      yo = -xo * nTan;
+    if ((rayAngle < (PI / 2)) || (rayAngle > (3*PI/2))) { // ray looking right
+      rayX = (((int)playerX >> 6) << 6) + 64;
+      rayY = (playerX - rayX) * nTan + playerY;
+      xOffset = 64;
+      yOffset = -xOffset * nTan;
     }
 
-    if (ra == 0 || ra == PI) { // ray directly up or down
-      rx = px;
-      ry = py;
+    if (rayAngle == 0 || rayAngle == PI) { // ray directly up or down
+      rayX = playerX;
+      rayY = playerY;
       dof = 8;
     }
 
     while (dof < 8) {
-      mx = (int)(rx)>>6;
-      my = (int)(ry)>>6;
-      mp = my * mapX + mx;
+      mapXPos = (int)(rayX)>>6;
+      mapYPos = (int)(rayY)>>6;
+      mapArrPos = mapYPos * mapX + mapXPos;
 
-      if (mp >= 0 && mp < mapX * mapY && map[mp] == 1) {// hit a wall
-        vx = rx;
-        vy = ry;
-        disV = dist(px, py, vx, vy, ra);
+      if (mapArrPos >= 0 && mapArrPos < (mapX * mapY) && map[mapArrPos] == 1) { // hit a vertical wall
+        vertRayX = rayX;
+        vertRayY = rayY;
+        rayDistVer = dist(playerX, playerY, vertRayX, vertRayY);
         dof = 8;
       } else {
-        rx += xo;
-        ry += yo;
+        rayX += xOffset;
+        rayY += yOffset;
         dof += 1; // next line
       }
     }
 
-    if (disV < disH) {
-      rx = vx;
-      ry = vy;
-      disT = disV;
+    if (rayDistVer < rayDistHor) {
+      rayX = vertRayX;
+      rayY = vertRayY;
+      distance = rayDistVer;
       glColor3f(0.55, 0.45, 0.40);
     }
 
-    if (disH < disV) {
-      rx = hx;
-      ry = hy;
-      disT = disH;
+    if (rayDistHor < rayDistVer) {
+      rayX = horRayX;
+      rayY = horRayY;
+      distance = rayDistHor;
       glColor3f(0.65, 0.6, 0.55);
     }
 
     // draw rays
     glLineWidth(1);
     glBegin(GL_LINES);
-    glVertex2i(px, py);
-    glVertex2i(rx, ry);
+    glVertex2i(playerX, playerY);
+    glVertex2i(rayX, rayY);
     glEnd();
 
     // draw 3d walls
-    float ca = ra - pa;
-    if (ca < 0) { ca += (2 * PI); }
-    if (ca > (2 * PI)) { ca -= (2 * PI); }
-    disT = disT*cos(ca); 
+    float correctedAngle = rayAngle - playerAngle;
+    if (correctedAngle < 0) { correctedAngle += (2 * PI); }
+    if (correctedAngle > (2 * PI)) { correctedAngle -= (2 * PI); }
+    float correctedDistance = distance * cos(correctedAngle);
 
-    float lineH = (mapS * 320) / disT;
-    if (lineH > 320) { lineH = 320; }
-    float lineO = 160 - lineH / 2;
+    float lineHeight = (mapSize * 320) / correctedDistance;
+    if (lineHeight > 320) { lineHeight = 320; }
+    float lineOffset = 160 - lineHeight / 2;
 
     glLineWidth(8);
     glBegin(GL_LINES);
-    glVertex2i(r * 8 + 530, lineO);
-    glVertex2i(r * 8 + 530, lineH + lineO);
+    glVertex2i(ray * 8 + 530, lineOffset);
+    glVertex2i(ray * 8 + 530, lineHeight + lineOffset);
     glEnd();
 
-    ra += DR;
-    if (ra < 0) { ra += 2 * PI; }
-    if (ra > 2 * PI) { ra -= 2 * PI; }
+    rayAngle += DEGREE;
+    if (rayAngle < 0) { rayAngle += 2 * PI; }
+    if (rayAngle > 2 * PI) { rayAngle -= 2 * PI; }
   }
 }
 
 void display() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   drawMap2D();
-  drawRays2D();
+  castRays();
   drawPlayer();
   glutSwapBuffers(); // swaps our buffers around
 }
@@ -222,25 +227,25 @@ void display() {
 void buttons(unsigned char key, int x, int y) {
   switch (key) {
   case 'd':
-    pa += 0.1;
-    if (pa > 2*PI) { pa -= 2 * PI; } // 2 * pi = 360Degrees 
-    pdx = cos(pa) * 5;
-    pdy = sin(pa) * 5;
+    playerAngle += 0.1;
+    if (playerAngle > 2 * PI) { playerAngle -= 2 * PI; } // 2 * pi = 360Degrees 
+    playerDeltaX = cos(playerAngle) * 5;
+    playerDeltaY = sin(playerAngle) * 5;
     break;
   case 'a':
-    pa -= 0.1;
-    if (pa < 0) { pa += 2 * PI; }
-    pdx = cos(pa) * 5;
-    pdy = sin(pa) * 5;
+    playerAngle -= 0.1;
+    if (playerAngle < 0) { playerAngle += 2 * PI; }
+    playerDeltaX = cos(playerAngle) * 5;
+    playerDeltaY = sin(playerAngle) * 5;
     
     break;
   case 'w':
-    px += pdx;
-    py += pdy;
+    playerX += playerDeltaX;
+    playerY += playerDeltaY;
     break;
   case 's':
-    px -= pdx;
-    py -= pdy;
+    playerX -= playerDeltaX;
+    playerY -= playerDeltaY;
     break;
   }
   glutPostRedisplay();
@@ -248,17 +253,19 @@ void buttons(unsigned char key, int x, int y) {
 
 void init() {
   glClearColor(0.2, 0.2, 0.2, 0);
-  gluOrtho2D(0, WIDTH, HEIGHT, 0);
+  gluOrtho2D(0, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, 0);
   // set player pos
-  px=400; py=300;
-  pdx = cos(pa) * 5; pdy = sin(pa) * 5;
+  playerX = 400;
+  playerY = 300;
+  playerDeltaX = cos(playerAngle) * 5;
+  playerDeltaY = sin(playerAngle) * 5;
 }
 
 int main(int argc, char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-  glutInitWindowSize(WIDTH, HEIGHT);
-  glutCreateWindow("raycaster");
+  glutInitWindowSize(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+  glutCreateWindow("Raycaster");
   init();
   glutDisplayFunc(display);
   glutKeyboardFunc(buttons);
